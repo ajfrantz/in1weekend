7227732875b1747315bd623d1fc7b16473aeeff7
AJ Frantz <aj@ajfrantz.com>
2018-12-27 21:41:41 -0500

    Chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 mod vec3;
+mod ray;
 
 use self::vec3::Vec3;
+use self::ray::Ray;
+
+fn color(r: &Ray) -> Vec3 {
+    let unit_direction = r.direction.unit();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Vec3::new(1., 1., 1.) + t * Vec3::new(0.5, 0.7, 1.)
+}
 
 fn main() {
     let nx = 200;
     let ny = 100;
     print!("P3\n{} {}\n255\n", nx, ny);
 
+    let lower_left_corner = Vec3::new(-2., -1., -1.);
+    let horizontal = Vec3::new(4., 0., 0.);
+    let vertical = Vec3::new(0., 2., 0.);
+    let origin = Vec3::new(0., 0., 0.);
+
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let color = 255.99 * Vec3::new_with_values(i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
+            let u = i as f32 / nx as f32;
+            let v = j as f32 / ny as f32;
+            let r = Ray { origin, direction: lower_left_corner + u * horizontal + v * vertical };
+            let color = 255.99 * color(&r);
             let ir = color.r() as i32;
             let ig = color.g() as i32;
             let ib = color.b() as i32;
--- src/vec3.rs
+++ src/vec3.rs
@@ -6,10 +6,7 @@ pub struct Vec3 {
 }
 
 impl Vec3 {
-    pub fn new() -> Vec3 {
-        Vec3 { e: [0., 0., 0.] }
-    }
-    pub fn new_with_values(x: f32, y: f32, z: f32) -> Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
         Vec3 { e: [x, y, z] }
     }
     pub fn x(&self) -> f32 {
