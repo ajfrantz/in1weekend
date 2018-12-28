81972713c2ba55f67b810344c64417f922e5e9c6
AJ Frantz <aj@ajfrantz.com>
2018-12-27 21:28:48 -0500

    Chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod vec3;
+
+use self::vec3::Vec3;
+
 fn main() {
     let nx = 200;
     let ny = 100;
@@ -5,12 +9,10 @@ fn main() {
 
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let r = i as f64 / nx as f64;
-            let g = j as f64 / ny as f64;
-            let b = 0.2;
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
+            let color = 255.99 * Vec3::new_with_values(i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
+            let ir = color.r() as i32;
+            let ig = color.g() as i32;
+            let ib = color.b() as i32;
             println!("{} {} {}", ir, ig, ib);
         }
     }
---
+++ src/vec3.rs
@@ -0,0 +1,152 @@
+use std::{fmt, ops};
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub struct Vec3 {
+    e: [f32; 3],
+}
+
+impl Vec3 {
+    pub fn new() -> Vec3 {
+        Vec3 { e: [0., 0., 0.] }
+    }
+    pub fn new_with_values(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { e: [x, y, z] }
+    }
+    pub fn x(&self) -> f32 {
+        self.e[0]
+    }
+    pub fn y(&self) -> f32 {
+        self.e[1]
+    }
+    pub fn z(&self) -> f32 {
+        self.e[2]
+    }
+    pub fn r(&self) -> f32 {
+        self.e[0]
+    }
+    pub fn g(&self) -> f32 {
+        self.e[1]
+    }
+    pub fn b(&self) -> f32 {
+        self.e[2]
+    }
+    pub fn squared_norm(&self) -> f32 {
+        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
+    }
+    pub fn norm(&self) -> f32 {
+        self.squared_norm().sqrt()
+    }
+    pub fn unit(&self) -> Vec3 {
+        *self / self.norm()
+    }
+    pub fn dot(&self, rhs: &Vec3) -> f32 {
+        self.e[0] * rhs.e[0] + self.e[1] * rhs.e[1] + self.e[2] * rhs.e[2]
+    }
+    pub fn cross(&self, rhs: &Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                (self.e[1] * rhs.e[2] - self.e[2] * rhs.e[1]),
+                -(self.e[0] * rhs.e[2] - self.e[2] * rhs.e[0]),
+                (self.e[0] * rhs.e[1] - self.e[1] * rhs.e[0]),
+            ],
+        }
+    }
+}
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} {}", self.x(), self.y(), self.z())
+    }
+}
+
+impl ops::Neg for Vec3 {
+    type Output = Vec3;
+    fn neg(self) -> Vec3 {
+        Vec3 {
+            e: [-self.e[0], -self.e[1], -self.e[2]],
+        }
+    }
+}
+
+impl ops::Add for Vec3 {
+    type Output = Vec3;
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0] + other.e[0],
+                self.e[1] + other.e[1],
+                self.e[2] + other.e[2],
+            ],
+        }
+    }
+}
+
+impl ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Vec3) {
+        self.e[0] += other.e[0];
+        self.e[1] += other.e[1];
+        self.e[2] += other.e[2];
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Vec3;
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0] - other.e[0],
+                self.e[1] - other.e[1],
+                self.e[2] - other.e[2],
+            ],
+        }
+    }
+}
+
+impl ops::SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Vec3) {
+        self.e[0] -= other.e[0];
+        self.e[1] -= other.e[1];
+        self.e[2] -= other.e[2];
+    }
+}
+
+impl ops::Mul<f32> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, scalar: f32) -> Vec3 {
+        Vec3 {
+            e: [self.e[0] * scalar, self.e[1] * scalar, self.e[2] * scalar],
+        }
+    }
+}
+
+impl ops::Mul<Vec3> for f32 {
+    type Output = Vec3;
+    fn mul(self, vec: Vec3) -> Vec3 {
+        vec * self
+    }
+}
+
+impl ops::MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, scalar: f32) {
+        self.e[0] *= scalar;
+        self.e[1] *= scalar;
+        self.e[2] *= scalar;
+    }
+}
+
+impl ops::Div<f32> for Vec3 {
+    type Output = Vec3;
+    fn div(self, scalar: f32) -> Vec3 {
+        Vec3 {
+            e: [self.e[0] / scalar, self.e[1] / scalar, self.e[2] / scalar],
+        }
+    }
+}
+
+impl ops::DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, scalar: f32) {
+        self.e[0] /= scalar;
+        self.e[1] /= scalar;
+        self.e[2] /= scalar;
+    }
+}
